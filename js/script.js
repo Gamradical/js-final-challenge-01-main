6b1c84cbb6755e46de3f308c0aaff0ff514af9ae
Maureen_g <gamradicalsix@gmail.com>
2023-05-11 13:57:46 -0500

    Created function and event listener to display random images

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 const button = document.querySelector("button");
+button.addEventListener("click", function() {
+    getImage();
+});
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
 const getImage = async function () {
     const res = await fetch ("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+   // console.log(images);
+    selectRandomImage(images);
 };
-getImage();
+
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    //console.log(randomIndex); Confirming number is called
+    //console.log(randomImage); Shows random image info/tracking
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
